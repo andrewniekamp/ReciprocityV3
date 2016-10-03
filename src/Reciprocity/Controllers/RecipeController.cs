d9e2175c1dbcd57ea1038ff5216e26819c7f4ba1
andrewniekamp <andrew.f.niekamp@gmail.com>
2016-10-03 12:24:41 -0700

    add recipe controller with method which will output list of recipes to the default view

---
+++ src/Reciprocity/Controllers/RecipeController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Reciprocity.Models;
+using System.Linq;
+
+namespace Reciprocity.Controllers
+{
+    public class RecipeController : Controller
+    {
+        private ReciprocityDbContext db = new ReciprocityDbContext();
+        public IActionResult Index()
+        {
+            return View(db.Recipes.ToList());
+        }
+    }
+}
