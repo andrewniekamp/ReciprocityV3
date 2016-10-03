de6820c55de6f5a2ffafdf1577f72a65dc65c5f4
andrewniekamp <andrew.f.niekamp@gmail.com>
2016-10-03 14:31:55 -0700

    add account controller with necessary identity information

---
+++ src/Reciprocity/Controllers/AccountController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Identity;
+using Reciprocity.Models;
+
+namespace Reciprocity.Controllers
+{
+    public class AccountController : Controller
+    {
+        private readonly ReciprocityDbContext _db;
+        private readonly UserManager<ApplicationUser> _userManager;
+        private readonly SignInManager<ApplicationUser> _signInManager;
+
+        public AccountController(
+            ReciprocityDbContext db,
+            UserManager<ApplicationUser> userManager,
+            SignInManager<ApplicationUser> signInManager)
+        {
+            _db = db;
+            _userManager = userManager;
+            _signInManager = signInManager;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
