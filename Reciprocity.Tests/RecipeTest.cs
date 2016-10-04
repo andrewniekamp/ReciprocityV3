d62acabfd0e9eda2aa3504140b9b4b5ba6feed89
andrewniekamp <andrew.f.niekamp@gmail.com>
2016-10-03 17:07:12 -0700

    add initial test - failing by default

---
+++ Reciprocity.Tests/RecipeTest.cs
@@ -0,0 +1,21 @@
+﻿using Reciprocity.Models;
+using Xunit;
+
+namespace Reciprocity.Tests
+{
+    public class RecipeTest
+    {
+        [Fact]
+        public void GetTitleTest()
+        {
+            //arrange
+            var recipe = new Recipe();
+
+            //act
+            var result = recipe.Title;
+
+            //assert
+            Assert.Equal("Chocolate Chip Cookies", result);
+        }
+    }
+}
