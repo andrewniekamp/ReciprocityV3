8da25a93dbd4293d1dafe557d8867dc3a94e7e2b
andrewniekamp <andrew.f.niekamp@gmail.com>
2016-10-03 17:28:11 -0700

    add recipe repository interface and implement upon EFrecipe repository

---
+++ src/Reciprocity/Models/Repositories/EFRecipeRepository.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Reciprocity.Models.Repositories
+{
+    public class EFRecipeRepository : IRecipeRepository
+    {
+        ReciprocityDbContext db = new ReciprocityDbContext();
+
+        public IQueryable<Recipe> Recipe
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        public Recipe Edit(Recipe recipe)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Remove(Recipe recipe)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Recipe Save(Recipe recipe)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ src/Reciprocity/Models/Repositories/IRecipeRepository.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Reciprocity.Models.Repositories
+{
+    public interface IRecipeRepository
+    {
+        IQueryable<Recipe> Recipe { get; }
+        Recipe Save(Recipe recipe);
+        Recipe Edit(Recipe recipe);
+        void Remove(Recipe recipe);
+    }
+}
