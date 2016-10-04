1a23793edb11a031a009671be8d116ca7be3df6d
andrewniekamp <andrew.f.niekamp@gmail.com>
2016-10-03 17:20:27 -0700

    add test for home controller

---
+++ Reciprocity.Tests/ModelTests/HomeControllerTest.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Reciprocity.Controllers;
+using Reciprocity.Models;
+using Xunit;
+
+namespace Reciprocity.Tests.ModelTests
+{
+    public class HomeControllerTest
+    {
+        [Fact]
+        public void Get_ViewResult_Index_Test()
+        {
+            //arrange
+            HomeController controller = new HomeController();
+
+            //act
+            var result = controller.Index();
+
+            //assert
+            Assert.IsType<ViewResult>(result);
+        }
+    }
+}
